a65b4a19119c6a5f0a32344b73e3ef941c79faeb
Nelson De Abreu <141588178+NelsonJDAS@users.noreply.github.com>
2024-10-31 10:12:36 +0000

    Version 1.0

---
+++ src/js/component/Regla.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Regla = () => {
+  return (
+    <h2 className="fw-bold text-white normas">
+      Toca el semaforo para interactuar con las luces
+    </h2>
+  );
+};
+
+export default Regla;
